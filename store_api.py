99ccedefc1c1647b3fccc418dc5cb7749d242bf8
ekwarodominic <dominicnoelchris@gmail.com>
2018-10-18 14:04:36 -0700

    Added a get-all-products method to return a single product [finishes #161256533]

--- store_api.py
+++ store_api.py
@@ -36,3 +36,15 @@ def get_all_products():
 def productgood(productId):
     item = [product for product in goods if product['id'] == productId]
     return jsonify({'item': item[0]})
+
+
+@app.route('/storemanager/v1/products/', methods=['POST'])
+def create_product():
+    product = {
+             'id': goods[-1]['id'] + 1,
+             'category': request.json['category'],
+             'name': request.json['name'],
+             'description': request.json['description']
+               }
+    goods.append(product)
+    return jsonify({'goods': product}), 201
